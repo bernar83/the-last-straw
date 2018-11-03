431791ad952b92d0988c9b8cfcbaf4b44e01479c
Adrian Bernardo <nabernardo93@gmail.com>
2018-11-03 12:36:10 -0700

    Add inital server file

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,34 +1,42 @@
-import React, {Component} from 'react';
-import TextField from '@material-ui/core/TextField';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
 import Typography from "@material-ui/core/Typography";
-import Button from '@material-ui/core/Button';
+import Button from "@material-ui/core/Button";
 
 class Register extends Component {
-    state = {
-        username: '',
-        password: ''
-    };
+  state = {
+    username: "",
+    password: ""
+  };
 
-    handleChange = name => event => {
-        this.setState({
-            [name]: event.target.value
-        });
-    };
+  handleChange = name => event => {
+    this.setState({
+      [name]: event.target.value
+    });
+  };
 
-    render() {
-        return (
-            <div>
-                <Typography variant="h3" gutterBottom>
-                    Register
-                </Typography>
-                <form>
-                    <TextField label="Username" value={this.state.username} onChange={this.handleChange} />
-                    <TextField label="Password" value={this.state.password} onChange={this.handleChange} />
-                    <Button variant="contained">Register</Button>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <Typography variant="h3" gutterBottom>
+          Register
+        </Typography>
+        <form>
+          <TextField
+            label="Username"
+            value={this.state.username}
+            onChange={this.handleChange}
+          />
+          <TextField
+            label="Password"
+            value={this.state.password}
+            onChange={this.handleChange}
+          />
+          <Button variant="contained">Register</Button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default Register;
