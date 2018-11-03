72ee386db6f747274b59e1a21d7a48234660bd12
Adrian Bernardo <nabernardo93@gmail.com>
2018-11-03 08:20:08 -0700

    Add initial login and register pages

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,7 +1,34 @@
 import React, {Component} from 'react';
+import TextField from '@material-ui/core/TextField';
+import Typography from "@material-ui/core/Typography";
+import Button from '@material-ui/core/Button';
 
-const Register = () => (
-    <h1>Register</h1>
-);
+class Register extends Component {
+    state = {
+        username: '',
+        password: ''
+    };
+
+    handleChange = name => event => {
+        this.setState({
+            [name]: event.target.value
+        });
+    };
+
+    render() {
+        return (
+            <div>
+                <Typography variant="h3" gutterBottom>
+                    Register
+                </Typography>
+                <form>
+                    <TextField label="Username" value={this.state.username} onChange={this.handleChange} />
+                    <TextField label="Password" value={this.state.password} onChange={this.handleChange} />
+                    <Button variant="contained">Register</Button>
+                </form>
+            </div>
+        );
+    }
+}
 
 export default Register;
