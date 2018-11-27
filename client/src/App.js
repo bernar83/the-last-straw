127490aed1538a2152f3c06116a4b8f8d32a252e
Adrian Bernardo <nabernardo93@gmail.com>
2018-11-26 21:40:24 -0800

    Remove malicious dependency

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
-import Landing from './components/Landing'
-import Register from './components/Register';
-import Login from './components/Login';
-import Profile from './components/Profile';
+import Landing from "./components/Landing";
+import Register from "./components/Register";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 
 class App extends Component {
   render() {
@@ -14,9 +14,9 @@ class App extends Component {
       <Router>
         <div>
           <Route exact path="/" component={Landing} />
-          <Route path="/register" component={Register} />
-          <Route path="/login" component={Login} />
-          <Route path="/profile" component={Profile} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/profile" component={Profile} />
         </div>
       </Router>
     );
